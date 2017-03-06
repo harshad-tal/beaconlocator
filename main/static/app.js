44bb12be3b83736a0fbc3d7e4e08dc2a40c030b6
harshad-tal <harshad.mulmuley@talentica.com>
2017-03-06 16:56:47 +0530

    image upload

--- main/static/app.js
+++ main/static/app.js
@@ -1,9 +1,10 @@
 var frozen_markers = [];
 var current_marker;
 var map = $("#map");
+var dialog;
 
 $(document).ready(function() {
-    var dialog;
+    upload_image_to_ls();
     load_db_beacons();
     load_ls_beacons();
 
@@ -24,35 +25,32 @@ $(document).ready(function() {
         },
         close: function() {},
         position: {
-            my: "right",
+            my: "right 10",
             of: $('.marker')
         }
     });
 
-    $('#map').click(function(e) {
-        x = e.pageX - ( $(window).width() / 10 ) - 24;
-        y = e.pageY - 48
-        $('.marker').css('left', x).css('top', y).show();
-        $("#x_position").val(x);
-        $("#y_position").val(y);
-        dialog.dialog("open");
-    });
+    $('#map').click(fp_click_handler);
 });
 
 
 function addBeacon() {
     var csrftoken = getCookie('csrftoken');
+    img = document.getElementById('map');
+    width_ratio = img.naturalWidth / img.clientWidth;
+    height_ratio = img.naturalHeight / img.clientHeight;
 
     add_to_local_storage({
         "beacon_id": $("#beacon_id").val(),
         "beacon_name": $("#beacon_name").val(),
-        "x_position": parseInt($("#x_position").val()),
-        "y_position": parseInt($("#y_position").val())
+        "x_position": parseInt($("#x_position").val() * width_ratio),
+        "y_position": parseInt($("#y_position").val() * height_ratio)
     });
 }
 
 function add_to_local_storage(data){
     var beacons = [];
+
     if (data.beacon_id !== ''){
         if(localStorage.getItem('beacons') == null){
             beacons.push(data);
@@ -62,7 +60,7 @@ function add_to_local_storage(data){
             beacons.push(data);
             write_local_storage('beacons', beacons);
         }
-        add_beacon_marker();
+        add_beacon_marker(data.beacon_id);
     } else {
         console.log('blank input');
     }
@@ -114,12 +112,16 @@ function load_ls_beacons(){
 function load_markers(beacons){
     var markers = $("#markers");
     var marker = $(".marker");
+    img = document.getElementById('map');
+    width_ratio = img.naturalWidth / img.clientWidth;
+    height_ratio = img.naturalHeight / img.clientHeight;
     if(beacons){
         for(i=0; i<beacons.length; ++i){
             new_marker = marker.clone();
+            new_marker.attr("id", "marker-"+beacons[i].beacon_id);
             new_marker.attr("class", "frozen_marker");
             new_marker.attr(
-            "style", "position: absolute; left: "+beacons[i].x_position+"px; top:" + beacons[i].y_position +"px"
+            "style", "cursor:pointer;position: absolute; left: "+beacons[i].x_position/width_ratio+"px; top:" + beacons[i].y_position/height_ratio +"px"
             );
             markers.append(new_marker);
         }
@@ -141,12 +143,13 @@ function load_db_beacons(){
     });
 }
 
-function add_beacon_marker() {
+function add_beacon_marker(beacon_id) {
     $("#beacon_id").val('');
     $("#beacon_name").val('');
 
     frozen_markers.push($('.marker'));
     new_marker = $(".marker").clone();
+    $('.marker').attr("id", "marker-"+beacon_id);
     $('.marker').attr("class", "frozen_marker");
     $('.marker').attr("src", "/static/main/frozen-marker-icon.png")
     $("#markers").append(new_marker);
@@ -167,3 +170,42 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+function fp_click_handler(e){
+    x = e.pageX - ( $(window).width() / 10 ) - 24;
+    y = e.pageY - 48
+    $('.marker').css('left', x).css('top', y).show();
+    $("#x_position").val(x);
+    $("#y_position").val(y);
+    dialog.dialog("open");
+}
+
+function upload_image_to_ls(){
+    var img = new Image();
+    img.src = localStorage.floorplan;
+    img.id = "map";
+    img.height = $(window).height() * 0.8;
+    img.width = $(window).width() * 0.8;
+
+    $('.imagearea').html(img);
+
+    $("body").on("change",".img_input",function(){
+        var fileInput = $(this)[0];
+        var file = fileInput.files[0];
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var img = new Image();
+            img.src = reader.result;
+            img.id = "map";
+            img.height = $(window).height() * 0.8;
+            img.width = $(window).width() * 0.8;
+            localStorage.floorplan = reader.result;
+
+            $(".imagearea").html(img);
+            $('#map').click(fp_click_handler);
+        }
+        reader.readAsDataURL(file);
+    });
+}
+
